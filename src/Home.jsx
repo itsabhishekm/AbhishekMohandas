4da65ff1e7ea38a65b95071993fc6db4a46daaee
itsabhishekm <mabhishek2817@gmail.com>
2024-08-22 22:54:23 +0530

    feat: add home section

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="row">
+        <div className="col md-10">
+            <div className="wrapper">
+                <div className="box"></div>
+            </div>
+        </div>
+        <div className="col md-2" id='intro'>
+            <b>
+            <h1>I'm</h1>
+            <h1>Abhishek Mohandas</h1>
+            <h2>I am a passionate developer, who is also secretly a wanna-be researcher ; )</h2></b>
+        </div>
+    </div>
+  )
+}
